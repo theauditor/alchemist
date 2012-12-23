5fa8b87af63c20e30504d393eba01ec5ea020731
Administrator <admin@local.host>
2012-12-23 14:44:19 +0530

    Model window addition complete

--- xadmin1942/Cpanel.php
+++ xadmin1942/Cpanel.php
@@ -7,13 +7,71 @@ include_once 'libadd.php';
 <html>
     <head>
         <script>
+            var $cn, $sc, $ec, $ic1, $ic2;
+
+ function spca(st)
+{
+     var iChars = "!@#$%^&*()+=-[]\\\';,./{}|\":<>?~_"; 
+   for (var i = 0; i < st.length; i++) 
+   {
+  	
+        if (iChars.indexOf(st.charAt(i)) != -1) 
+        {
+  	   	return true;
+        }
+   }
+   return false;
+}
+
+
+
+
 $(function(){
 
 $("#acc").accordion();
   
+  
+                $("#AddCatB").click(function(){
+                    $("#addModal").modal('show');
+                });
+                
+            $("#AddB").click(function(){
+                $cn =    $("#cnamI").val();
+                $sc = $("#clsI1").val();
+                $ec = $("#clsI2").val();
+                $ic1 = $("#In1").val();
+                $ic2 = $("#In2").val();
+                
+                jQuery.get("ccat.php",{n: $cn, sc: $sc, ec: $ec, i1:$ic1,i2: $ic2}, function(data){
+                    $("#hider2").animate({opacity:0,height:0}, 1000);
+                    $("#hider").animate({opacity:0,height:0}, 1000);
+                    if(data==0)
+                        {
+                            
+                            $("#hider").animate({opacity:1,height:30}, 1000);
+                            
+                            
+                        }else
+                            {
+                                $("#acc").append(data).accordion('destroy').accordion();
+                                
+                                $("#cnamI").val('');
+                                $("#clsI1").val('');
+                                $("#clsI2").val('');
+                                $("#In1").val('');
+                                $("#In2").val('');
+                                $("#hider2").animate({opacity:1,height:30}, 1000);
+                                
+                               
+                                
+                            }
+                });
+            });
+  
 });      
 
-$( "#P" ).spinner();
+   
+   
  
     
 </script>
@@ -108,7 +166,7 @@ $( "#P" ).spinner();
                     ?>
                          
                     </div>
-                        <a href="#addModal" role="button" class="btn btn-link" data-toggle="modal">+ Add Category</a>
+                        <button  id="AddCatB" type="button" class="btn btn-link" >+ Add Category</button>
                     </div>
                     <div>
                     
@@ -123,7 +181,7 @@ $( "#P" ).spinner();
     <form>
         <fieldset>
         <div>
-        <input style="height:30px;width:60%" type="text" name="cnamI" maxlength="30" placeholder="Category Name"/>
+        <input style="height:30px;width:60%" type="text" id="cnamI" maxlength="30" placeholder="Category Name"/>
         </div>
             <b>Classes:</b>
         </br> From <select id="cls1I" placeholder="Class" name="cls" class="sel">
@@ -160,12 +218,25 @@ $( "#P" ).spinner();
         <div>
        <input style="height:30px;width:60%" id="In2" type="text" placeholder="Incharge Two"/>
         </div>
+        <div id="hider" style="height:0px;opacity: 0">
+                        <div class="ui-state-error">
+                            <span class="ui-icon ui-icon-alert" style="float:left;margin-left: 3px;"></span>
+                            <b>ERROR : </b>Failed to create a category.
+                        </div>
+                        </div>
+        <div id="hider2" style="height:0px;opacity: 0">
+                        <div class="alert alert-success">
+                            <span class="ui-icon ui-icon-alert" style="float:left;margin-left: 3px;"></span>
+                            <b>Congratulation : </b>Category Added.
+                        </div>
+                        </div>
+        
         </fieldset>
     </form>
 </div>
 <div class="modal-footer">
 <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-<button class="btn btn-primary">Save changes</button>
+<button class="btn btn-inverse" id="AddB">Add +</button>
 </div>
 </div>
                     <!--- ADD CAT MODAL END-->
@@ -176,6 +247,30 @@ $( "#P" ).spinner();
             </div>
             <div id="push"></div>
         </div>
+        
+        <!--- ACTIVE MOD --->
+        <div id="randMod" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+<h3 id="myModalLabel">Add Category</h3>
+</div>
+<div class="modal-body" id="Rmod">
+    
+</div>
+<div class="modal-footer">
+<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+<button class="btn btn-inverse" id="AddB">Add +</button>
+</div>
+</div>
+        
+        <!---END ACTIVE MOD --->
+        
+        
+        
+        
+        
+        
+        
           <div  id="footer">
     <div style="float:right;overflow:hidden;padding-top:20px">
                 <h7><b>Powered By</b></h7>
---
+++ xadmin1942/ccat.php
@@ -0,0 +1,5 @@
+<?php
+include_once 'logver.php';
+
+die("<h3>HELOOOO</h3><div>defwsfe</div>");
+?>
